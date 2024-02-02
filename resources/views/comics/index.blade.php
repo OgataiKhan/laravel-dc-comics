d81777aaea7ee2b144cb99c4398826c9332b186c
OgataiKhan <badellino.kevin@gmail.com>
2024-02-02 09:04:17 +0100

    feat: add cards to list view

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,10 +7,18 @@
 @section('main')
     <main>
         <div class="container">
-            <p><a href="{{ route('comics.create') }}">Add new comic</a></p>
-            <ul>
+            <p class="text-center"><a href="{{ route('comics.create') }}">Add new comic</a></p>
+            <ul class="row gy-4">
                 @foreach ($comics as $comic)
-                    <li>{{ $comic->title }} - <a href="{{ route('comics.show', $comic->id) }}" alt="Comic">View details</a></li>
+                    <li class="col-md-3 d-flex">
+                        <div class="card d-flex flex-column" style="width: 18rem;">
+                            <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
+                            <div class="card-body flex-grow-1">
+                                <h5 class="card-title">{{ $comic->title }}</h5>
+                                <p class="card-text">{{ $comic->series }}</p>
+                                <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">View details</a>
+                            </div>
+                        </div>
                 @endforeach
             </ul>
         </div>
