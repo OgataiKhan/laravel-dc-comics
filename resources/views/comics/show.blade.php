f0b12574458a02edd38fa62b2c58b8e7390ee94a
OgataiKhan <badellino.kevin@gmail.com>
2024-02-02 09:20:00 +0100

    feat: details page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -9,6 +9,31 @@
         <div class="container">
             <h2>{{ $comic->title }}</h2>
 
+            <div class="details d-flex justify-content-between">
+                <div class="info">
+                    <h4 class="series">{{ $comic->series }}</h4>
+                    <p class="type bold"> {{ Str::ucfirst($comic->type) }}</p>
+                    <p class="price"><span class="bold">Price:</span>: {{ $comic->price }}</p>
+                    <p class="sale-date"><span class="bold">Sale date:</span> {{ $comic->sale_date }}</p>
+                    <p class="artists"><span class="bold">Artists:</span>
+                        @php $artists = explode(',', $comic->artists); @endphp
+                        @foreach ($artists as $artist)
+                            {{ $loop->first ? '' : ', ' }}{{ $artist }}
+                        @endforeach
+                    </p>
+                    <p class="writers"><span class="bold">Writers:</span>
+                      @php $writers = explode(',', $comic->writers); @endphp
+                      @foreach ($writers as $writer)
+                          {{ $loop->first ? '' : ', ' }}{{ $writer }}
+                      @endforeach
+                  </p>
+                  <p class="description">{{ $comic->description }}</p>
+                </div>
+                <div class="cover-box">
+                    <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                </div>
+            </div>
+
             <p><a href="{{ route('comics.index') }}">Go to comics list</a></p>
         </div>
     </main>
